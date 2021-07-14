97cec4de7623d51dfab780f1766d33a668816aa3
Piotr Kosiński <kosinskipiotr.98@gmail.com>
2021-07-14 23:42:53 +0200

    Add views test link routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import MainLayout from './components/layout/MainLayout/MainLayout';
+import HomePage from './components/views/HomePage/HomePage';
+import Login from './components/views/Login/Login';
+import Ordering from './components/views/Ordering/Ordering';
+import Tables from './components/views/Tables/Tables';
+import Kitchen from './components/views/Kitchen/Kitchen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={'/panel'}>
+      <MainLayout>
+        <Switch>
+          <Route exact path={process.env.PUBLIC_URL + '/'} component={HomePage} />
+          <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+          <Route path={process.env.PUBLIC_URL + '/ordering'} component={Ordering} />
+          <Route path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
+          <Route path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+        </Switch>
+      </MainLayout>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/layout/MainLayout/MainLayout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PageNav from '../PageNav/PageNav'
+
+const MainLayout = (props) => (
+  <main>
+    <PageNav />
+    {props.children}
+  </main>
+);
+
+export default MainLayout;
