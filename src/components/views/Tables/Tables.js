8db0880f1b8aff87a922e3f4aae6e5fb7ea7eb16
Piotr Kosiński <kosinskipiotr.98@gmail.com>
2022-01-20 17:36:40 +0100

    Code-split TableColumn & Add conditional rendering of TableCell

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -21,6 +21,8 @@ import { grey } from '@mui/material/colors';
 //import Booking from '../Booking/Booking';
 //import Event from '../Event/Event';
 
+import { tables } from '../../../settings';
+
 class Tables extends React.Component {
   static propTypes = {
     eventsRepeatLoading: PropTypes.shape({
@@ -90,28 +92,31 @@ class Tables extends React.Component {
         </Container>
       );
     } else {
+      const openHour = '12:00';
+      const workingHours = 12;
+      const tableRows = workingHours * 2 + 1;
+
       const TableHours = ({ id }) => {
         if (id % 2 !== 0) {
-          const startHour = '12:00';
           let hoursToAdd = 0;
 
           id === 1 ? hoursToAdd = 0 : hoursToAdd = (id - 1) / 2
 
-          const displayedHour = DateTime.fromISO(startHour).plus({hours: hoursToAdd}).toLocaleString(DateTime.TIME_SIMPLE);
+          const displayedHour = DateTime.fromISO(openHour).plus({hours: hoursToAdd}).toLocaleString(DateTime.TIME_SIMPLE);
 
           return (
             <Box
-              height='40px'
+              height='45px'
               borderBottom='1px solid'
               borderColor={grey[400]}
               position='relative'
             >
               <Box
-                width='40px'
+                width='45px'
                 height='15px'
                 backgroundColor='white'
                 position='absolute'
-                top='26px'
+                top='31px'
               >
                 <Typography variant='caption' mr={1}>
                   {displayedHour} 
@@ -122,83 +127,91 @@ class Tables extends React.Component {
         } else {
           return (
             <Box
-              height='40px'
+              height='45px'
               border='none'
             />
           );
         }
       };
 
-      const TableCell = () => {
+      const TableCell = ({ id, table, borderRight }) => {
         return (
-          <Box
-            height='40px'
-            borderLeft='1px solid'
-            borderBottom='1px solid'
-            borderColor={grey[400]}
-          />
+          <>
+            {id === 1 ?
+              <Box
+                height='45px'
+                borderLeft='1px solid'
+                borderBottom='1px solid'
+                borderRight={borderRight ? '1px solid' : 'none'}
+                borderColor={grey[400]}
+                display='flex'
+                justifyContent='center'
+                alignItems='center'
+              >
+                <Typography variant='body2'>{table.label}</Typography>
+              </Box>
+              :
+              <Box
+                height='45px'
+                borderLeft='1px solid'
+                borderBottom='1px solid'
+                borderRight={borderRight ? '1px solid' : 'none'}
+                borderColor={grey[400]}
+              />}
+          </>
+        );
+        
+      };
+
+      const TableHoursColumn = () => {
+        return (
+          <Grid item xs={3}>
+            {[...Array(tableRows)].map(
+              (value, i) => (
+                <TableHours key={i} id={i + 1} />
+              )
+            )}
+          </Grid>
         );
       };
 
-      const TableCellEnd = () => {
+      const TableColumn = ({ table, borderRight }) => {
         return (
-          <Box
-            height='40px'
-            borderLeft='1px solid'
-            borderBottom='1px solid'
-            borderRight='1px solid'
-            borderColor={grey[400]}
-          />
+          <Grid item xs={3}>
+            {[...Array(tableRows)].map(
+              (value, i) => (
+                <TableCell key={i} id={i + 1} table={table} borderRight={borderRight} />
+              )
+            )}
+          </Grid>
         );
       };
-
-      const tableCellRows = 25;
 
       return (
         <Container>
           <Box justifyContent='center' my={2}>
             <Grid container marginX='auto'>
-              <Grid item xs={3}>
-                {[...Array(tableCellRows)].map(
-                  (value, i) => (
-                    <TableHours id={i + 1} key={i} />
-                  )
-                )}
-              </Grid>
-              <Grid item xs={3}>
-                {[...Array(tableCellRows)].map(
-                  (value, i) => (
-                    <TableCell id={i + 1} key={i} />
-                  )
-                )}
-              </Grid>
-              <Grid item xs={3}>
-                {[...Array(tableCellRows)].map(
-                  (value, i) => (
-                    <TableCell id={i + 1} key={i} />
-                  )
-                )}
-              </Grid>
-              <Grid item xs={3}>
-                {[...Array(tableCellRows)].map(
-                  (value, i) => (
-                    <TableCellEnd id={i + 1} key={i} />
-                  )
-                )}
-              </Grid>
+              <TableHoursColumn />
+              {tables.map((table, i) => {
+                return (
+                  <React.Fragment key={i}>
+                    <TableColumn table={table} borderRight={table === tables[tables.length -1]} />
+                  </React.Fragment>
+                );
+              })}
             </Grid>
           </Box>
           
           {/*
-          <h2>Tables view</h2>
-          <Link to={process.env.PUBLIC_URL + '/tables/booking/123abc'}>Booking</Link>
-          <Link to={process.env.PUBLIC_URL + '/tables/event/321abc'}>Event</Link>
-          <div>
-            <Switch>
-              <Route path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={Booking} />
-              <Route path={process.env.PUBLIC_URL + '/tables/event/:id'} component={Event} />
-            </Switch>
-          </div>
+            <h2>Tables view</h2>
+            <Link to={process.env.PUBLIC_URL + '/tables/booking/123abc'}>Booking</Link>
+            <Link to={process.env.PUBLIC_URL + '/tables/event/321abc'}>Event</Link>
+            <div>
+              <Switch>
+                <Route path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={Booking} />
+                <Route path={process.env.PUBLIC_URL + '/tables/event/:id'} component={Event} />
+              </Switch>
+            </div>
           */}
         </Container>
       );
