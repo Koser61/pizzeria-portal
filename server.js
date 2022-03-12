7804edc7653b835cc513c9e2e93b246cd317ff1f
Koser61 <kosinskipiotr.98@gmail.com>
2022-03-12 13:32:23 +0100

    Fix repeat param & dom updating after submit in client website booking

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const middlewares = jsonServer.defaults({
 });
 const port = process.env.PORT || 3131;
 
-server.get(/\/panel.*/, (req,res) =>{
+server.get(/\/panel.*/, (req,res) => {
   if(req.url == '/panel'){
     req.url += '/';
   }
